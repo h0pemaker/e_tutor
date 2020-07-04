368a8a2547c423a3f13e49eedef87a486fc40882
Harsh Verma <harshvermayoyo@gmail.com>
2020-07-04 18:22:35 +0530

    initial successful commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:etutor/services/authentication.dart';
+import 'package:etutor/pages/root_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'eTutor',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
